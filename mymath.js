7d2b5e9a55da7e43e31bc2eee6c31f7627196879
Anat <anat@alooma.io>
2017-11-13 19:58:01 +0200

    first npm package

---
+++ mymath.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+
+   add: function (num1 , num2) {
+        return num1 + num2
+      },
+
+   subtract : function (num1,num2) {
+     return num1 - num2
+   },
+
+   multiply : function ( num1, num2) {
+     return num1 * num2
+   },
+
+   divite : function (num1 , num2) {
+       return num1/num2
+   }
+
+}
